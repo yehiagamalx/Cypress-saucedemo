2e7f7f54d1f08048f9e828d367a48592a40941f5
Yehia Gamal <93436604+yehiagamalx@users.noreply.github.com>
2023-07-15 19:59:56 +0300

    create cart scenarios

---
+++ cypress/e2e/02-Cart.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe("testing cart scenarios", () => {
+  beforeEach(() => {
+    cy.fixture("usersData").as("users");
+    cy.visit("/");
+    cy.get(".login_logo").should("contain", "Swag Labs");
+    cy.get("@users").then((users) => {
+      cy.get('[data-test="username"]').type(users.standard.userName);
+      cy.get('[data-test="password"]').type(users.standard.password);
+    });
+    cy.get('[data-test="login-button"]').click();
+    cy.get(".title").should("contain", "Products");
+  });
+
+  it("Add item to the cart", () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get(".shopping_cart_badge").should("exist");
+  });
+
+  it("Remove item from the cart", () => {
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    cy.get(".shopping_cart_badge").should("exist");
+    cy.get('[data-test="remove-sauce-labs-backpack"]').click();
+    cy.get(".shopping_cart_badge").should("not.exist");
+  });
+});
