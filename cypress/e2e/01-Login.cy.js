e2e7b5ae6f5077aaf5e7d2b8e8038332d48e37db
Yehia Gamal <93436604+yehiagamalx@users.noreply.github.com>
2023-07-15 19:39:09 +0300

    re-organized login scenarios

--- cypress/e2e/01-Login.cy.js
+++ cypress/e2e/01-Login.cy.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
-describe("Negative login scenario", () => {
+describe("testing login scenarios", () => {
   beforeEach(() => {
     cy.fixture("usersData").as("users");
   });
 
-  it("Login with valid user", () => {
+  it("Login with invalid user", () => {
     cy.visit("/");
     cy.get(".login_logo").should("contain", "Swag Labs");
     cy.get("@users").then((users) => {
@@ -14,5 +14,16 @@ describe("Negative login scenario", () => {
     });
     cy.get('[data-test="login-button"]').click();
     cy.get('[data-test="error"]').should("contain", "Epic sadface");
+  });
+
+  it("Login with valid user", () => {
+    cy.visit("/");
+    cy.get(".login_logo").should("contain", "Swag Labs");
+    cy.get("@users").then((users) => {
+      cy.get('[data-test="username"]').type(users.standard.userName);
+      cy.get('[data-test="password"]').type(users.standard.password);
+    });
+    cy.get('[data-test="login-button"]').click();
+    cy.get(".title").should("contain", "Products");
   });
 });
